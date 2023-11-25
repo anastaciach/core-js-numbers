b703ac90c479114da444b2d9e3073ff74b77d0c4
Anastasia <aachuchkalova@gmail.com>
2023-11-25 23:25:21 +0700

    feat:added problem solving(latest ToNumber)

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -221,8 +221,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Number.isFinite(+value)) return +value;
+  return def;
 }
 
 /**
